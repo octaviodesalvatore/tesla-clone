6b19ff5cb25704e34f8b2a6d5c1c8e733be9331f
unknown <octaviodesalvatore@gmail.com>
2021-10-08 14:23:20 -0300

    Sections

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImg={backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
-      <ButtonGroup>
-        <LeftButton>Customn Order</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {/* Si el right button existe, que se muestre. 
+          Esto es para que cuando no exista el boton.
+           En los accesorios quede centrado el unico
+          Boton que seria el leftBtn */}
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 }
@@ -24,13 +37,23 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("./images/model-s.jpg");
+  background-image: ${(props) => `url("/images/${props.bgImg}")`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
 `;
-const ButtonGroup = styled.div``;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 const LeftButton = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
@@ -42,6 +65,24 @@ const LeftButton = styled.div`
   align-items: center;
   border-radius: 100px;
   opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
 `;
 
-const RightButton = styled(LeftButton)``;
+//Herada los estilos del Right Button
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div``;
